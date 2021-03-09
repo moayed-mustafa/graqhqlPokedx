f5de9517f9bf15e724ccee339d69fdc0e6b04356
moayed-mustafa <moayed.abrida@yahoo.com>
2021-03-10 00:15:49 +0400

    graphql stuff

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,4 +1,15 @@
 import React from "react";
+import { gql, useQuery } from '@apollo/react-hooks'
+
+
 export default function PokemonsContainer() {
+  const { data } = useQuery(gql`
+query{
+  pokemons(first:10){
+    name
+  }
+}`
+  )
+  console.log(data)
   return <h1>Pokemon container</h1>;
 }
